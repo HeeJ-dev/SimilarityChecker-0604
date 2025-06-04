2deac78413c559bc0a28ae85c40c34a9057fec84
hj <hj@example.com>
2025-06-04 16:44:39 +0900

    [refactoring] TestCode 중복제거, return 값 int로 변환하여 버림처리

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,5 +1,5 @@
 public class SimilarityChecker {
-    public double getScoreStringLength(String s1, String s2) {
+    public int getScoreStringLength(String s1, String s2) {
         int s1Length = s1.length();
         int s2Length = s2.length();
         int gapLength = Math.abs(s1.length() - s2.length());
@@ -9,8 +9,8 @@ public class SimilarityChecker {
         if(s1Length >= s2Length * 2 || s2Length >= s1Length * 2)
             return 0;
         if(s1Length > s2Length)
-            return  Math.round(((1-(double) gapLength/ s2Length)*60));
+            return  (int)((1-(double) gapLength/ s2Length)*60);
         else
-            return  Math.round(((1-(double) gapLength/ s1Length)*60));
+            return  (int)((1-(double) gapLength/ s1Length)*60);
     }
 }
