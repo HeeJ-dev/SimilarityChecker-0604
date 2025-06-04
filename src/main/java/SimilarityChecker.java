53b6e7dd9691ae96a5e5b9aa199508169869d458
hj <hj@example.com>
2025-06-04 16:24:42 +0900

    [feature] length

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,2 +1,18 @@
 public class SimilarityChecker {
+    public double getScoreStringLength(String s1, String s2) {
+        if(s1.length() == s2.length()){
+            return 60;
+        }
+        if(s1.length() == s2.length()*2 || s2.length() == s1.length()*2) {
+            return 0;
+        }
+        else {
+            if(s1.length()>s2.length()) {
+                return  Math.round(((1-(double)(s1.length()-s2.length())/s2.length())*60));
+            }
+            else {
+                return  Math.round(((1-(double)(s2.length()-s1.length())/s1.length())*60));
+            }
+        }
+    }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -1,5 +1,38 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityCheckerTest {
-
+    @Test
+    void testGetScoreStringLength100Score() {
+        SimilarityChecker similarityChecker = new SimilarityChecker();
+        String s1 = "ADS";
+        String s2 = "DSA";
+        double actual = similarityChecker.getScoreStringLength(s1, s2);
+        assertEquals(60, actual);
+    }
+    @Test
+    void testGetScoreStringLength0Score() {
+        SimilarityChecker similarityChecker = new SimilarityChecker();
+        String s1 = "A";
+        String s2 = "BB";
+        double actual = similarityChecker.getScoreStringLength(s1, s2);
+        assertEquals(0, actual);
+    }
+    @Test
+    void testGetScoreStringLengthPartialScoreLongerS1() {
+        SimilarityChecker similarityChecker = new SimilarityChecker();
+        String s1 = "AAABB";
+        String s2 = "BAA";
+        double actual = similarityChecker.getScoreStringLength(s1, s2);
+        assertEquals(20, actual);
+    }
+    @Test
+    void testGetScoreStringLengthPartialScoreLongerS2() {
+        SimilarityChecker similarityChecker = new SimilarityChecker();
+        String s1 = "AA";
+        String s2 = "AAE";
+        double actual = similarityChecker.getScoreStringLength(s1, s2);
+        assertEquals(30, actual);
+    }
 }
