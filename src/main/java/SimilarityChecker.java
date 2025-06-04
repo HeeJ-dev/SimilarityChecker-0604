2beac2ceca906fbab6b33f25cdf6a02c061a0f68
hj <hj@example.com>
2025-06-05 08:30:42 +0900

    [feature] alpha

--- src/main/java/SimilarityChecker.java
+++ src/main/java/SimilarityChecker.java
@@ -1,20 +1,41 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class SimilarityChecker {
     public int getScoreStringLength(String s1, String s2) {
         int s1Length = s1.length();
         int s2Length = s2.length();
         int gapLength = Math.abs(s1.length() - s2.length());
 
-        if(s1Length == s2Length)
+        if (s1Length == s2Length)
             return 60;
-        if(isMorethanTwiceLength(s1Length, s2Length))
+        if (isMorethanTwiceLength(s1Length, s2Length))
             return 0;
-        if(s1Length > s2Length)
-            return  (int)((1-(double) gapLength/ s2Length)*60);
+        if (s1Length > s2Length)
+            return (int) ((1 - (double) gapLength / s2Length) * 60);
         else
-            return  (int)((1-(double) gapLength/ s1Length)*60);
+            return (int) ((1 - (double) gapLength / s1Length) * 60);
     }
 
     private boolean isMorethanTwiceLength(int s1Length, int s2Length) {
         return s1Length >= s2Length * 2 || s2Length >= s1Length * 2;
+    }
+
+    public int getScoreAlpha(String s1, String s2) {
+        Set<Character> setS1 = new HashSet<>();
+        Set<Character> setS2 = new HashSet<>();
+        Set<Character> setTotal = new HashSet<>();
+
+        for(int i=0; i<s1.length(); i++) {
+            setS1.add(s1.charAt(i));
+            setTotal.add(s1.charAt(i));
+        }
+
+        for(int i=0; i<s2.length(); i++) {
+            setS2.add(s2.charAt(i));
+            setTotal.add(s2.charAt(i));
+        }
+
+        return (int)((double)(setS1.size() + setS2.size()-setTotal.size())/setTotal.size()*40.0);
     }
 }
--- src/test/java/SimilarityCheckerTest.java
+++ src/test/java/SimilarityCheckerTest.java
@@ -39,4 +39,68 @@ class SimilarityCheckerTest {
         int actual = similarityChecker.getScoreStringLength(s1, s2);
         assertEquals(30, actual);
     }
+
+    @Test
+    void testGetScoreAlpha100ScoreSameLength() {
+        String s1 = "ASD";
+        String s2 = "DSA";
+        int actual = similarityChecker.getScoreAlpha(s1, s2);
+        assertEquals(40, actual);
+    }
+    @Test
+    void testGetScoreAlpha100ScoreLongerS1() {
+        String s1 = "AAABB";
+        String s2 = "BA";
+        int actual = similarityChecker.getScoreAlpha(s1, s2);
+        assertEquals(40, actual);
+    }
+    @Test
+    void testGetScoreAlpha100ScoreLongerS2() {
+        String s1 = "BA";
+        String s2 = "AAABB";
+        int actual = similarityChecker.getScoreAlpha(s1, s2);
+        assertEquals(40, actual);
+    }
+    @Test
+    void testGetScoreAlpha0ScoreSameLength() {
+        String s1 = "AA";
+        String s2 = "BB";
+        int actual = similarityChecker.getScoreAlpha(s1, s2);
+        assertEquals(0, actual);
+    }
+    @Test
+    void testGetScoreAlpha0ScoreLongerS1() {
+        String s1 = "AA";
+        String s2 = "B";
+        int actual = similarityChecker.getScoreAlpha(s1, s2);
+        assertEquals(0, actual);
+    }
+    @Test
+    void testGetScoreAlpha0ScoreLongerS2() {
+        String s1 = "A";
+        String s2 = "BB";
+        int actual = similarityChecker.getScoreAlpha(s1, s2);
+        assertEquals(0, actual);
+    }
+    @Test
+    void testGetScoreAlphaPartialScoreSameLength() {
+        String s1 = "AAC";
+        String s2 = "AAE";
+        int actual = similarityChecker.getScoreAlpha(s1, s2);
+        assertEquals(13, actual);
+    }
+    @Test
+    void testGetScoreAlphaPartialScoreLongerS1() {
+        String s1 = "AAE";
+        String s2 = "AA";
+        int actual = similarityChecker.getScoreAlpha(s1, s2);
+        assertEquals(20, actual);
+    }
+    @Test
+    void testGetScoreAlphaPartialScoreLongerS2() {
+        String s1 = "AA";
+        String s2 = "AAE";
+        int actual = similarityChecker.getScoreAlpha(s1, s2);
+        assertEquals(20, actual);
+    }
 }
